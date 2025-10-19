71942ec399000621c421aff78f1d9eff964e7de9
sahan chanuka <sahanchanuka90@gmail.com>
2025-10-19 13:47:52 +0530

    Update portfolio.js

--- my-portfolio/src/portfolio.js
+++ my-portfolio/src/portfolio.js
@@ -7,11 +7,7 @@ import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your
 
 // Splash Screen
 
-const splashScreen = {
-  enabled: true, // set false to disable splash screen
-  animation: splashAnimation,
-  duration: 2000 // Set animation duration as per your animation
-};
+
 
 // Summary And Greeting Section
 
